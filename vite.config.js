3906f26924404141fd297b2f10500fbe74cf6868
Emil <emiliansar@gmail.com>
2025-02-10 22:10:17 +0300

    add config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
   base: '/weather-app/'
-}
+})
